a412a29c19acc3ecfce5786f32962b2ad2337360
Léandre Daumont <digiz3d@users.noreply.github.com>
2019-10-22 23:40:31 +0200

    First push

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Dockerfile with 2 steps : one building
+# The last one is the one that we will use
+
+## Deps
+FROM node:latest AS build
+COPY . /tmp
+RUN cd /tmp && yarn install
+
+## Bin
+FROM build
+RUN mkdir -p /usr/bin
+COPY --from=build /tmp /usr/bin
+WORKDIR /usr/bin
+CMD ["yarn","start"]
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require("cors")
+
+const app = express();
+
+app.use(cors())
+app.use(express.json())
+app.use("/", (req,res) => {
+    res.send('Welcome.')
+})
+
+app.listen(process.env.PORT || 3000, process.env.HOSTNAME || "127.0.0.1", () => {
+    console.log(`listening. lol`)
+})
