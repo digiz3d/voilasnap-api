9c2c5d9a5e802cfa5f171b215dde5b8401ff6a7a
Léandre Daumont <digiz3d@users.noreply.github.com>
2019-10-22 23:58:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use("/", (req,res) => {
-    res.send('Welcome back.')
+    res.send('Welcome back. Again.')
 })
 
 app.listen(process.env.PORT || 3000, process.env.HOSTNAME || "127.0.0.1", () => {
