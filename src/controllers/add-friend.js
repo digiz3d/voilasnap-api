98090782c0a5daef52af38433c9c4884416a7f2f
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2020-05-15 22:39:54 +0200

    update -> updateOne

--- src/controllers/add-friend.js
+++ src/controllers/add-friend.js
@@ -8,19 +8,19 @@ export default async (req, res) => {
   try {
     if (res.locals.user.friends.received.map((id) => id.toString()).includes(user._id.toString())) {
       await Promise.all([
-        User.update(
+        User.updateOne(
           { _id: res.locals.userId },
           { $addToSet: { 'friends.list': user._id }, $pull: { 'friends.received': user._id } },
         ),
-        User.update(
+        User.updateOne(
           { _id: user._id },
           { $addToSet: { 'friends.list': res.locals.userId }, $pull: { 'friends.sent': res.locals.userId } },
         ),
       ])
     } else {
       await Promise.all([
-        User.update({ _id: res.locals.userId }, { $addToSet: { 'friends.sent': user._id } }),
-        User.update({ _id: user._id }, { $addToSet: { 'friends.received': res.locals.userId } }),
+        User.updateOne({ _id: res.locals.userId }, { $addToSet: { 'friends.sent': user._id } }),
+        User.updateOne({ _id: user._id }, { $addToSet: { 'friends.received': res.locals.userId } }),
       ])
     }
     return res.send({ success: true })
