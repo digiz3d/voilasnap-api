08eeca82fdcda69c62ec60847b8efcc551eca084
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2020-05-17 16:42:33 +0200

    search with regex

--- src/controllers/search-users.js
+++ src/controllers/search-users.js
@@ -2,7 +2,11 @@ import User from '../models/User'
 
 export default async (req, res) => {
   try {
-    const users = await User.find({ username: new RegExp(`^${req.body.username}`, 'i') }, { _id: 1, username: 1 })
+    const usernameString = typeof req.body.username === 'string' ? req.body.username : JSON.stringify(req.body.username)
+    const username = usernameString.replace(/[^a-zA-Z0-9]/g, '.')
+    const usernameRegex = new RegExp(`^${username}`, 'i')
+
+    const users = await User.find({ username: usernameRegex }, { _id: 1, username: 1 })
       .sort({ createdAt: 1 })
       .limit(15)
       .exec()
@@ -11,6 +15,7 @@ export default async (req, res) => {
 
     return res.send({ success: true, payload: users })
   } catch (err) {
+    console.log(err)
     return res.status(400).send({ error: true })
   }
 }
