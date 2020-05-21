e64bf2e12da04a12cf833b5302b42a64aa201cd3
Leandre Daumont <1338620+digiz3d@users.noreply.github.com>
2020-05-21 20:06:01 +0200

    increase json parsing limit

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ mongoose
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '50mb' }))
 
 if (process.env.NODE_ENV === 'development') {
   app.use(async (req, res, next) => {
